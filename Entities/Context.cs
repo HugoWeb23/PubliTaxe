1fa9ad58c6e9120a4ba787faccea3d3f34ca9a7d
HugoWeb23 <hugohourriez@live.be>
2021-11-09 08:12:00 +0100

    first commit

---
+++ Entities/Context.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Taxes.Entities
+{
+    public class MyContext : DbContext
+    {
+        public MyContext(DbContextOptions<MyContext> options) : base(options)
+        {
+        }
+
+    }
+}
