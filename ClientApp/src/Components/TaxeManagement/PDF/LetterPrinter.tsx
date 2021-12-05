ff575f8e4236d4a95ea7ccb640690bc015b695f5
HugoWeb23 <hugohourriez@live.be>
2021-12-05 11:58:11 +0100

    wip

--- ClientApp/src/Components/TaxeManagement/PDF/LetterPrinter.tsx
+++ ClientApp/src/Components/TaxeManagement/PDF/LetterPrinter.tsx
@@ -69,7 +69,7 @@ export const LetterPrinter = ({ entreprise, printData, tarifs }: ILetterPrinter)
         </View>
         <View style={styles.CityName}>
             <View>
-                <Text style={{fontSize: '14px'}}>Administration Communale</Text>
+                <Text style={{ fontSize: '14px' }}>Administration Communale</Text>
                 <Text style={styles.NormalText}>Service de la Recette</Text>
                 <Text style={styles.NormalText}>Rue de Courtrai, 63</Text>
                 <Text style={styles.NormalText}>B-7700 Mouscron</Text>
@@ -84,17 +84,27 @@ export const LetterPrinter = ({ entreprise, printData, tarifs }: ILetterPrinter)
         </View>
         <View style={styles.TransversalLogo}>
             <View>
-                <Image src={transversal} style={styles.TransversalLogoImg}/>
+                <Image src={transversal} style={styles.TransversalLogoImg} />
             </View>
-            <View style={{marginRight: '40mm'}}>
+            <View style={{ marginRight: '40mm' }}>
                 <Text style={styles.NormalText}>Mouscron, le {printData.print_date}</Text>
             </View>
         </View>
-        <View style={{marginTop: '10mm'}}>
+        <View style={{ marginTop: '10mm' }}>
             <Text style={styles.NormalText}>Madame, Monsieur,</Text>
         </View>
-        <View style={{marginTop: '10mm'}}>
-            <Text style={styles.NormalText}>Concerne : Taxe communale sur les panneaux publicitaires et les enseignes</Text>
+        <View style={{ marginTop: '10mm' }}>
+            <Text style={[styles.NormalText, { textDecoration: 'underline' }]}>Concerne : Taxe communale sur les panneaux publicitaires et les enseignes</Text>
+        </View>
+        <View style={{ marginTop: '5mm' }}>
+            <Text style={styles.NormalText}>Vous trouverez en annexe le formulaire de déclaration relatif aux panneaux et/ou enseignes publicitaires pour l'exercice 2022, <Text style={{ textDecoration: 'underline' }}>avec situation au 1er janvier de l'année</Text>.</Text>
+            <Text style={[styles.NormalText, { marginTop: '5mm' }]}>Voudriez-vous vérifier si ces données sont exactes et, éventuellement, les compléter en cas d'oubli ?</Text>
+            <Text style={[styles.NormalText, { marginTop: '5mm' }]}>Nous vous prions de bien vouloir nous retourner cette déclaration signée de préférence par mail à {printData.mail} ou au Service des Taxes de la ville de Mouscron, rue de Courtrai, 63 à 7700 Mouscron pour le <Text style={{ textDecoration: 'underline' }}>{printData.deadline}</Text> au plus tard et ce, même s'il n'y a aucun changement à la situation.</Text>
+            <Text style={[styles.NormalText, { marginTop: '5mm' }]}>Pour tout renseignement complémentaire, veuillez contacter</Text>
+            <View style={{marginLeft: '50mm'}}>
+                <Text style={styles.NormalText}>{printData.contact_person} au {printData.phone}</Text>
+                <Text style={[styles.NormalText, {marginLeft: '20mm'}]}>(du lundi au jeudi)</Text>
+            </View>
         </View>
     </Page>
 }
