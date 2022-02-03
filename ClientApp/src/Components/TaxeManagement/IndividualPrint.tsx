328033ecfa8a0bc04a35d04f0c3813a9c4a20a90
HugoWeb23 <hugohourriez@live.be>
2022-02-03 10:49:32 +0100

    Correction date d'impression par défaut

--- ClientApp/src/Components/TaxeManagement/IndividualPrint.tsx
+++ ClientApp/src/Components/TaxeManagement/IndividualPrint.tsx
@@ -20,7 +20,8 @@ export const IndividualPrint = ({ show, handleClose, tax, tarifs, currentFiscalY
   const Today = () => {
     const date = new Date()
     const month = date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1
-    return `${date.getFullYear()}-${month}-${date.getDate()}`
+    const day = date.getDate() < 10 ? '0' + date.getDate() : date.getDate()
+    return `${date.getFullYear()}-${month}-${day}`
 
   }
   const initialValues = ({
