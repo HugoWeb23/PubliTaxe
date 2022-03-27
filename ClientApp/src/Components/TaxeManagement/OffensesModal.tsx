ec717963179da9be668412bd847533b1ee6f3daf
HugoWeb23 <hugohourriez@live.be>
2022-03-27 21:16:40 +0200

    Confirmation suppression infraction

--- ClientApp/src/Components/TaxeManagement/OffensesModal.tsx
+++ ClientApp/src/Components/TaxeManagement/OffensesModal.tsx
@@ -3,7 +3,8 @@ import {
     Modal,
     Table,
     Alert,
-    Button
+    Button,
+    Card
 } from 'react-bootstrap'
 import { useEffect, useState } from 'react'
 import { ApiErrors, apiFetch } from '../../Services/apiFetch'
@@ -25,9 +26,10 @@ interface IOffensesModal {
 export const OffensesModal = ({ matricule, motifs, currentFiscalYear, isOpen, handleClose, onDelete }: IOffensesModal) => {
 
     const [history, setHistory] = useState<INotReceivedHistory[]>([])
+    const [confirmDelete, setConfirmDelete] = useState<{ show: boolean, notReceived: INotReceivedHistory }>({ show: false, notReceived: {} as INotReceivedHistory })
     const [loader, setLoader] = useState<boolean>(true)
 
-    const fetchData = async() => {
+    const fetchData = async () => {
         setLoader(true)
         const history = await apiFetch(`/notreceived/gethistory/${matricule}`)
         setHistory(history)
@@ -47,6 +49,7 @@ export const OffensesModal = ({ matricule, motifs, currentFiscalYear, isOpen, ha
             })
             onDelete(deleteNotReceived)
             setHistory(history => history.filter((h: INotReceivedHistory) => h.id !== notReceived.id))
+            setConfirmDelete({show: false, notReceived: {} as INotReceivedHistory})
             toast.success("L'infraction a été supprimée")
         } catch (e) {
             if (e instanceof ApiErrors) {
@@ -62,35 +65,46 @@ export const OffensesModal = ({ matricule, motifs, currentFiscalYear, isOpen, ha
         </Modal.Header>
         <Modal.Body>
             <div className="d-flex justify-content-end mb-3">
-                <Button size="sm" variant="success" onClick={() => fetchData()}><RefreshIcon/> Actualiser</Button>
+                <Button size="sm" variant="success" onClick={() => fetchData()}><RefreshIcon /> Actualiser</Button>
             </div>
             {loader === true && <Loader />}
             {(loader == false && history.length === 0) && <Alert variant="warning">Aucune infraction enregistrée</Alert>}
             {(loader == false && history.length !== 0) &&
-                <Table striped bordered hover>
-                    <thead>
-                        <tr>
-                            <th>Exercice</th>
-                            <th>Motif</th>
-                            <th>% Major</th>
-                            <th>Remarque</th>
-                            <th>Date</th>
-                            <th>Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {history.map((histo: INotReceivedHistory) => {
-                            return <tr key={histo.id}>
-                                <td>{histo.exercice}</td>
-                                <td>{motifs.find((motif: IMotif_majoration) => motif.id_motif == histo.motif_majorationId)?.libelle}</td>
-                                <td>{histo.pourcentage_majoration} %</td>
-                                <td>{histo.remarque}</td>
-                                <td>{histo.date}</td>
-                                <td>{currentFiscalYear.annee_exercice - parseInt(histo.exercice) < 2 && <Button size="sm" variant="danger" onClick={() => handleDelete(histo)}>Supprimer</Button>}</td>
+                <>
+                {confirmDelete.show && <Card border="danger" className="mb-3">
+                    <Card.Header>Confirmation de suppression</Card.Header>
+                    <Card.Body>
+                        Voulez-vous vraiment supprimer l'infraction de l'exercice 2022 ?
+                        <div className="mt-3">
+                            <Button size="sm" variant="success" onClick={() => handleDelete(confirmDelete.notReceived)}>Oui</Button> - <Button size="sm" variant="danger" onClick={() => setConfirmDelete({show: false, notReceived: {} as INotReceivedHistory})}>Non</Button>
+                        </div>
+                    </Card.Body>
+                </Card>}
+                    <Table striped bordered hover>
+                        <thead>
+                            <tr>
+                                <th>Exercice</th>
+                                <th>Motif</th>
+                                <th>% Major</th>
+                                <th>Remarque</th>
+                                <th>Date</th>
+                                <th>Action</th>
                             </tr>
-                        })}
-                    </tbody>
-                </Table>
+                        </thead>
+                        <tbody>
+                            {history.map((histo: INotReceivedHistory) => {
+                                return <tr key={histo.id}>
+                                    <td>{histo.exercice}</td>
+                                    <td>{motifs.find((motif: IMotif_majoration) => motif.id_motif == histo.motif_majorationId)?.libelle}</td>
+                                    <td>{histo.pourcentage_majoration} %</td>
+                                    <td>{histo.remarque}</td>
+                                    <td>{histo.date}</td>
+                                    <td>{currentFiscalYear.annee_exercice - parseInt(histo.exercice) < 2 && <Button size="sm" variant="danger" onClick={() => setConfirmDelete({show: true, notReceived: histo})}>Supprimer</Button>}</td>
+                                </tr>
+                            })}
+                        </tbody>
+                    </Table>
+                </>
             }
         </Modal.Body>
         <Modal.Footer>
